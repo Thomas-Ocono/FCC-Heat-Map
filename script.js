614f18c9626b4adaded65dd0aaba6c49cf853b99
Thomas O'Connor <thomas.Ocono@gmail.com>
2025-04-20 18:09:31 -0400

    Axis workin

---
+++ script.js
@@ -0,0 +1,89 @@
+const url =
+  "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/global-temperature.json";
+const req = new XMLHttpRequest();
+
+const main = (data) => {
+  const baseTemp = data.baseTemperature;
+  const monthlyVariance = data.monthlyVariance;
+
+  //make an array of years from the data, removing duplicates
+  let yearsArray = [];
+  monthlyVariance.forEach((element) => {
+    if (!yearsArray.includes(element.year)) {
+      yearsArray.push(element.year);
+    }
+  });
+
+  const height = 700;
+  const width = 1500;
+  const padding = 100;
+
+  let xScale = d3
+    .scaleLinear()
+    .domain([d3.min(yearsArray), d3.max(yearsArray)])
+    .range([padding, width - padding]);
+
+  let yScale = d3
+    .scaleLinear()
+    .domain([0.5, 12.5])
+    .range([height - padding, padding]);
+
+  const svg = d3
+    .select("body")
+    .append("svg")
+    .attr("height", height)
+    .attr("width", width)
+    .style("border", "5px solid black")
+    .style("border-radius", "5px")
+    .style("display", "block")
+    .style("margin", "auto");
+
+  let xAxis = d3.axisBottom(xScale).tickFormat(d3.format("d"));
+  svg
+    .append("g")
+    .call(xAxis)
+    .attr("id", "x-axis")
+    .attr("transform", "translate(0, " + (height - padding) + ")");
+
+  let yAxis = d3.axisLeft(yScale).tickFormat((d) => {
+    switch (d) {
+      case 1:
+        return "December";
+      case 2:
+        return "November";
+      case 3:
+        return "October";
+      case 4:
+        return "September";
+      case 5:
+        return "August";
+      case 6:
+        return "July";
+      case 7:
+        return "June";
+      case 8:
+        return "May";
+      case 9:
+        return "April";
+      case 10:
+        return "March";
+      case 11:
+        return "February";
+      case 12:
+        return "January";
+    }
+  });
+  svg
+    .append("g")
+    .call(yAxis)
+    .attr("id", "y-axis")
+    .attr("transform", "translate(" + padding + ", 0)");
+};
+
+//get data from url then run main function
+req.open("GET", url, true);
+req.onload = () => {
+  let data = JSON.parse(req.responseText);
+  main(data);
+};
+req.send();
