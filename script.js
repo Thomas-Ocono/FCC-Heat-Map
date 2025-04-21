468788b2444011a716f4dded90deeeadc17ee28f
Thomas O'Connor <thomas.Ocono@gmail.com>
2025-04-20 22:48:37 -0400

    13/18 passed, need legend and tooltip

--- script.js
+++ script.js
@@ -24,9 +24,9 @@ const main = (data) => {
     .range([padding, width - padding]);
 
   let yScale = d3
-    .scaleLinear()
-    .domain([1, 12])
-    .range([height - padding, padding]);
+    .scaleTime()
+    .domain([new Date(0, 0, 0, 0, 0, 0, 0), new Date(0, 12, 0, 0, 0, 0, 0)])
+    .range([padding, height - padding]);
 
   const svg = d3
     .select("body")
@@ -39,58 +39,44 @@ const main = (data) => {
     .style("margin", "auto");
 
   let xAxis = d3.axisBottom(xScale).tickFormat(d3.format("d"));
+  let yAxis = d3.axisLeft(yScale).tickFormat(d3.timeFormat("%B"));
   svg
     .append("g")
     .call(xAxis)
     .attr("id", "x-axis")
     .attr("transform", "translate(0, " + (height - padding) + ")");
-
-  let yAxis = d3.axisLeft(yScale).tickFormat((d) => {
-    switch (d) {
-      case 1:
-        return "December";
-      case 2:
-        return "November";
-      case 3:
-        return "October";
-      case 4:
-        return "September";
-      case 5:
-        return "August";
-      case 6:
-        return "July";
-      case 7:
-        return "June";
-      case 8:
-        return "May";
-      case 9:
-        return "April";
-      case 10:
-        return "March";
-      case 11:
-        return "February";
-      case 12:
-        return "January";
-    }
-  });
   svg
     .append("g")
     .call(yAxis)
     .attr("id", "y-axis")
-    .attr("transform", "translate(" + padding + ", 0)");
+    .attr("transform", "translate(" + padding + ")");
+
   svg
     .selectAll("rect")
     .data(monthlyVariance)
     .enter()
     .append("rect")
     .attr("class", "cell")
-    .attr("data-month", (d) => d.month)
+    .attr("data-month", (d) => d.month - 1)
     .attr("data-year", (d) => d.year)
     .attr("data-temp", (d) => baseTemp + d.variance)
-    .attr("width", (d, i) => (width - padding) / yearsArray.length)
-    .attr("height", (d, i) => (height - padding) / 12)
+    .attr("width", (width - padding) / yearsArray.length)
+    .attr("height", (height - 2 * padding) / 12)
     .attr("x", (d) => xScale(d.year))
-    .attr("y", (d) => yScale(d.month) - padding / 2);
+    .attr("y", (d) => yScale(new Date(0, d.month - 1, 0, 0, 0, 0, 0)))
+    .attr("fill", (d) => {
+      if (d.variance <= -1) {
+        return "SteelBlue";
+      }
+      if (d.variance <= 0) {
+        return "LightSteelBlue";
+      }
+      if (d.variance <= 1) {
+        return "orange";
+      } else {
+        return "crimson";
+      }
+    });
 };
 
 //get data from url then run main function
